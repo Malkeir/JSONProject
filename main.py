756c0fa33953cb25842b3ea13c6103b84116dc82
malkeir <badorr@charter.net>
2021-06-17 16:21:05 -0400

    started json project and created all the create and add fucntions for a list and dictionary. working on the parsing section now for json files

--- main.py
+++ main.py
@@ -3,14 +3,50 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+JSONFILE = []
+ObjectAllowence = False
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def EmptyList():
+    listL =[]
+    return listL
+
+def AddingList(val , listL):
+    listL.append(val)
+    return listL
+
+def EmptyDiction():
+    dic = {}
+    return dic
+
+def CreateDiction(key,pair):
+    emptyDic ={}
+    emptyDic.update({key:pair})
+    return emptyDic
+def AddDiction(key,pair,DICTION):
+
+    DICTION.update({key:pair})
+    return DICTION
+
+def ObjectInObject(list):
+
+    for
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
+def ParsingJson(file):
+    jsonInfo = EmptyDiction()
+    listHolder = []
+    for x in file.readlines():
+        listHolder.append(x)
+    print(listHolder)
+    ObjectInObject(listHolder)
+
+def main():
+    DIR = "TestJson.json"
+    file = open(DIR)
+    ParsingJson(file)
+
+
+main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
